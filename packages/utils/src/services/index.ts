1794ff3e41c40ad44ada6e15f18eb39d2e438873
Marian Bucuresteanu <marian.bucuresteanu.hs@gmail.com>
2022-11-22 01:39:45 +0200

    apiUtil, hydration

--- packages/utils/src/services/index.ts
+++ packages/utils/src/services/index.ts
@@ -1,20 +1,53 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { HYDRATE } from "next-redux-wrapper";
+
 export const pokemonApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://pokeapi.co/api/v2/",
   }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
   tagTypes: [],
   endpoints: (endpointBuilder) => ({
     getPokemonByName: endpointBuilder.query({
       query: (name) => `pokemon/${name}`,
     }),
-    getPokemonList: endpointBuilder.query({
-      query: () => `pokemon/`,
+    getPaginatedPokemonsList: endpointBuilder.query<
+      {
+        results: Array<{ name: string }>;
+        count: number;
+      },
+      { page: number; perPage: number }
+    >({
+      query: ({ page, perPage }) =>
+        `pokemon/?offset=${page * perPage}&limit=${page * perPage + perPage}`,
+    }),
+    getAllPokemonsList: endpointBuilder.query<
+      {
+        results: Array<{ name: string }>;
+        count: number;
+      },
+      void
+    >({
+      query: () => `pokemon/?offset=0&limit=10000`,
     }),
   }),
 });
 
-export const { useGetPokemonByNameQuery, useGetPokemonListQuery } = pokemonApi;
+export const pokemonApiUtil: typeof pokemonApi.util = pokemonApi.util;
 
-export const { getPokemonByName, getPokemonList } = pokemonApi.endpoints;
+export const {
+  useGetPokemonByNameQuery,
+  useGetPaginatedPokemonsListQuery,
+  useGetAllPokemonsListQuery,
+} = pokemonApi;
+
+export const {
+  getPokemonByName,
+  getPaginatedPokemonsList,
+  getAllPokemonsList,
+} = pokemonApi.endpoints;
