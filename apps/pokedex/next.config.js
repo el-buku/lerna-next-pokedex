7e6ec8e3fbe2848e1981569c9a7ac35f4c594ff9
Marian Bucuresteanu <marian.bucuresteanu.hs@gmail.com>
2022-11-23 13:37:32 +0200

    next/image config

--- apps/pokedex/next.config.js
+++ apps/pokedex/next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withImages = require('next-images');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental:{
+  compiler:{
     emotion:true
-  }
+  },
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
