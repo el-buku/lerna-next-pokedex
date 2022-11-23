fa73dbcf06f46e90ad40d716e6199d93f7f412fd
Marian Bucuresteanu <marian.bucuresteanu.hs@gmail.com>
2022-11-23 17:24:24 +0200

    fetch SSR polyfill

--- apps/pokedex/pages/_app.tsx
+++ apps/pokedex/pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { wrapper } from "pokedex-utils";
+import fetch, { Headers, Request, Response } from "node-fetch";
+
+if (!globalThis.fetch) {
+  Object.assign(globalThis, {
+    fetch,
+    Headers,
+    Request,
+    Response,
+  });
+}
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
