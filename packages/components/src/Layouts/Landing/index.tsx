4de7ab30a8af91d542523f65b296c1ce0c106a21
Marian Bucuresteanu <marian.bucuresteanu.hs@gmail.com>
2022-11-23 18:51:22 +0200

    height fix

--- packages/components/src/Layouts/Landing/index.tsx
+++ packages/components/src/Layouts/Landing/index.tsx
@@ -14,7 +14,7 @@ import { AbsoluteCenter, MarginAuto } from "../../styles/base";
 import { LoadingLayout } from "..";
 
 const GridContainer = css`
-  height: 100%;
+  height: calc(100% - 350px);
   ${MarginAuto}
 `;
 
